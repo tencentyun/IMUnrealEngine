15570e4317065ff372246c7ad148de671c15af1b
clavieniu <clavieniu@tencent.com>
2021-12-28 11:39:26 +0800

    新增readme

--- IM_Demo/Source/IM_Demo/IM_Demo.Build.cs
+++ IM_Demo/Source/IM_Demo/IM_Demo.Build.cs
@@ -15,7 +15,7 @@ public class IM_Demo : ModuleRules
 
 		loadDebugDll(Target);
 
-		loadTIMDll(Target);
+		loadTIMSDK(Target);
 	}
 
 	// 加载生成userSig 的framework。该方案仅适用于调试Demo，正式上线前请将 UserSig 计算代码和密钥迁移到您的后台服务器上，以避免加密密钥泄露导致的流量盗用。
@@ -48,15 +48,12 @@ public class IM_Demo : ModuleRules
 		}
 	}
 
-	private void loadTIMDll(ReadOnlyTargetRules Target)
-	{
+	private void loadTIMSDK(ReadOnlyTargetRules Target) {
 		string _TIMSDKPath = Path.GetFullPath(Path.Combine(ModuleDirectory, "TIMSDK"));
-    bEnableUndefinedIdentifierWarnings = false;
+    	bEnableUndefinedIdentifierWarnings = false;
 		PublicIncludePaths.Add(Path.Combine(_TIMSDKPath, "include"));
-		if (Target.Platform == UnrealTargetPlatform.Android)
-		{
+		if (Target.Platform == UnrealTargetPlatform.Android) {
             PrivateDependencyModuleNames.AddRange(new string[] { "Launch" });
-            
             AdditionalPropertiesForReceipt.Add(new ReceiptProperty("AndroidPlugin", Path.Combine(ModuleDirectory, "TIMSDK", "Android", "APL_armv7.xml")));
 			
 			string Architecture = "armeabi-v7a";
@@ -64,23 +61,19 @@ public class IM_Demo : ModuleRules
 			// string Architecture = "armeabi";
 			PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory,"TIMSDK", "Android", Architecture, "libImSDK.so"));
 		}
-		else if (Target.Platform == UnrealTargetPlatform.IOS)
-		{
+		else if (Target.Platform == UnrealTargetPlatform.IOS) {
 			// PublicAdditionalFrameworks.Add(new UEBuildFramework("ImSDK",_TIMSDKPath+"/ios/ImSDK.framework.zip", ""));
 			PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory,"TIMSDK/iOS/ImSDK_CPP.framework/ImSDK_CPP"));
 			RuntimeDependencies.Add(Path. Combine(ModuleDirectory,"TIMSDK/iOS/ImSDK_CPP.framework/Info.plist"), UnrealBuildTool.StagedFileType.SystemNonUFS);
 			RuntimeDependencies.Add(Path.Combine(ModuleDirectory,"TIMSDK/iOS/ImSDK_CPP.framework/ImSDK_CPP"),UnrealBuildTool.StagedFileType.SystemNonUFS);
 		}
-		else if(Target.Platform == UnrealTargetPlatform.Mac)
-		{
+		else if(Target.Platform == UnrealTargetPlatform.Mac) {
 			// PublicFrameworks.Add(Path.Combine(_TIMSDKPath, "Mac", "Release","ImSDKForMac.framework"));
 			PublicFrameworks.Add(Path.Combine(_TIMSDKPath, "Mac", "Release","ImSDKForMac_CPP.framework"));
 		}
-		else if (Target.Platform == UnrealTargetPlatform.Win64)
-		{
+		else if (Target.Platform == UnrealTargetPlatform.Win64) {
 			PublicAdditionalLibraries.Add(Path.Combine(_TIMSDKPath, "win64", "Release","ImSDK.lib"));
 			PublicDelayLoadDLLs.Add(Path.Combine(_TIMSDKPath, "win64", "Release", "ImSDK.dll"));
-
 			RuntimeDependencies.Add("$(BinaryOutputDir)/ImSDK.dll", Path.Combine(_TIMSDKPath, "win64", "Release", "ImSDK.dll"));
 		}
 	}
